8e1a617fecead70313b94ff66f7c6f513edece8c
Jean Charles Alinquant <jcdev@Jeans-MacBook-Pro.local>
2024-03-22 11:17:42 +0000

    Extracted Order class to new file

---
+++ DP_Interpreter/Order.cs
@@ -0,0 +1,46 @@
+using System.Text.RegularExpressions;
+
+namespace DesignPatterns.Interpreter;
+
+public class Order
+{
+    // Grammar Representation
+    const string optionalSpace = " ?";
+    const string qty = "x(?<qty>\\d+)" + optionalSpace;
+    const string product = "'(?<product>[\\w ]+)'" + optionalSpace;
+    const string source = "from (?<source>\\w+)";
+    const string orderCommand = "order" + optionalSpace + qty + product + source;
+
+    static Regex _regex = new Regex(orderCommand);
+
+    public Order(int qty, string product, string source)
+    {
+        Qty = qty;
+        Product = product;
+        Source = source;
+    }
+
+    public int Qty { get; }
+    public string Product { get; }
+    public string Source { get; }
+
+    // Interpreter
+    public static Order? Parse(string command)
+    {
+        var match = _regex.Match(command);
+        if (!match.Success) return null;
+
+        var qty = int.Parse(match.Groups["qty"].Value);
+        var product = match.Groups["product"].Value;
+        var source = match.Groups["source"].Value;
+
+        return new Order(qty, product, source);
+    }
+
+    public void Dump()
+    {
+        Console.WriteLine($"Product: {Product}");
+        Console.WriteLine($"Source: {Source}");
+        Console.WriteLine($"Quantity: {Qty}");
+    }
+}
